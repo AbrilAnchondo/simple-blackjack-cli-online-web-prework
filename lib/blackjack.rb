03a5d2d947dea5c3e3547de28e512ccd84de03b6
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-03-27 23:42:10 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -37,7 +37,15 @@ def initial_round
   return sum
  end
 
-def hit?(current_total)
+def hit?(current_total)prompt_user #ask the user if they want to hit or stay
+  user_input = get_user_input #set a variable to use for your if/else statement
+  if user_input == "h"
+    card_total += deal_card #increase your former total by the new number given by dealing another card
+  elsif user_input == "s"
+    card_total #return the number and prompt user to hit again
+  else
+    invalid_command
+  end
   # code hit? here
   prompt_user
   input = get_user_input
